2177a70fafc1fe478fa31a8126582a6f9a6fe91e
julio mol <juliomolina17@gmail.com>
2022-05-26 18:58:58 -0300

    primer subida

---
+++ Clase_5/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+string opcion = "S";
+
+while(opcion == "S"){
+    Console.Clear();
+    Console.Write("Ingrese su nombre: ");
+    string nombre1 = Console.ReadLine();
+    Console.WriteLine("Hola " + nombre1);
+    Console.Write("Desea continuar? Ingrese S o N : ");
+    opcion = Console.ReadLine();
+}
+
+if(opcion=="N"){
+    Console.WriteLine("Programa finalizado correctamente");
+}
+else{
+    Console.WriteLine("Opcion no valida");
+}
+
+/*
+Escribir un programa que haga lo siguiente:
+
+1) Borrar la pantalla.
+2) Pedir el nombre de una persona.
+3) Saludarlo con un texto que diga "¡Hola [NombreIngresado]!"
+4) Preguntar si se quiere continuar.
+5) Si la respuesta es "S" repetir desde el punto 1.
+6) Si la respuesta es "N" finalizar el programa mostrando un mensaje que diga "Programa finalizado correctamente".
+7) Si la respuesta no es ni "S" ni "N" que tambien finalice el programa, pero mostrando un mensaje que diga "Opcion no valida".
+
+*/
---
+++ Clase_6/Program.cs
@@ -0,0 +1,57 @@
+﻿int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 21);
+
+int contador = 0;
+int numero;
+do
+{
+    Console.Write("\n");
+    Console.Write("Ingrese un numero entre 1 y 20: ");
+    numero = int.Parse(Console.ReadLine());
+    if (numero == numeroSecreto)
+    {
+        Console.Write("Felicitaciones, has adivinado el número secreto que era: " + numeroSecreto);
+        contador+=1;
+        Console.Write("\n");
+        Console.Write("Lo has logrado en: " + contador + " intentos!!");
+        Console.Write("\n");
+    }
+    else if (numero>numeroSecreto)
+    {
+        Console.Write("Numero muy grande, intente de nuevo");
+        contador+=1;
+    }
+    else if (numero<numeroSecreto)
+    {
+        Console.Write("Numero muy chico, intente de nuevo");
+        contador+=1;
+    }
+
+} while (numeroSecreto != numero);
+
+/*
+1) Generar un número secreto
+aleatorio con la siguiente instruccion:
+
+// Genera un numero entero aleatorio de 1 a 20
+int numeroSecreto = new
+Random(DateTime.Now.Millisecond).Next(1, 21);
+
+2) Pedir al usuario que ingrese un número y que
+intente adivinar el número que eligió la computadora.
+
+3) Si el numero ingresado por el usuario es mayor
+al número secreto, avisarle que es muy grande y que intente de nuevo y que
+vuelva al paso 2.
+
+4) Si el numero ingresado es menor al número
+secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
+
+5) Si el número ingresado coincide con el número
+secreto, sacar el siguiente mensaje:
+
+"Felicitaciones, has adivinado el número
+secreto que era:  [numeroSecreto]"
+"Lo has logrado en [n] intentos!!"
+
+Y finalizar el programa.
+*/
